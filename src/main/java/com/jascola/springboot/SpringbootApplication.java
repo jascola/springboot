c592bc5c17226667c5df1ae463d59a62688d6113
jascola <1947857236@qq.com>
2018-10-15 15:44:09 +0800

    实验springBoot自动装配

--- src/main/java/com/jascola/springboot/SpringbootApplication.java
+++ src/main/java/com/jascola/springboot/SpringbootApplication.java
@@ -1,5 +1,6 @@
 package com.jascola.springboot;
 
+import com.jascola.demo.DataService;
 import com.jascola.model.dao.UserDao;
 import com.jascola.model.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @SpringBootApplication
@@ -17,8 +19,16 @@ import java.util.List;
 public class SpringbootApplication  {
 	@Autowired
 	UserDao dao;
+	//DataService自动装配，不需要配置bean
+	DataService service;
+	@Resource
+	public void setService(DataService service)	{
+		 this.service = service;
+	}
+
 	@RequestMapping(value = "/")
 	public String fff(Model model){
+		System.out.println(service.sayHello());
 		model.addAttribute("hah",dao.selectAll());
 		return "index";
 	}
