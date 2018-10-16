c09ef5c3e7fe2425916986fc5210271258a41fef
jascola <1947857236@qq.com>
2018-10-16 16:46:30 +0800

    spring security

--- src/main/java/com/jascola/springboot/conf/WebSecurityConfig.java
+++ src/main/java/com/jascola/springboot/conf/WebSecurityConfig.java
@@ -1,7 +1,5 @@
 package com.jascola.springboot.conf;
 
-import com.jascola.util.MD5Util;
-import org.apache.tomcat.util.security.MD5Encoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -15,34 +13,35 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private static final Logger logger  = LoggerFactory.getLogger(WebSecurityConfig.class);
+    private static final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
+
     @Override
-    protected void configure(HttpSecurity http){
+    protected void configure(HttpSecurity http) {
         try {
-            super.configure(http);
+            http.authorizeRequests().antMatchers("/**").hasRole("ADMIN").and().formLogin().and().httpBasic();
         } catch (Exception e) {
-            logger.info(e.getLocalizedMessage(),e);
+            logger.info(e.getLocalizedMessage(), e);
         }
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth){
+    protected void configure(AuthenticationManagerBuilder auth) {
         try {
 
             auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder()).withUser("jascola").
                     password(new BCryptPasswordEncoder().encode("123456")).roles("ADMIN");
         } catch (Exception e) {
-            logger.info(e.getLocalizedMessage(),e);
+            logger.info(e.getLocalizedMessage(), e);
 
         }
     }
 
     @Override
-    public void configure(WebSecurity web){
+    public void configure(WebSecurity web) {
         try {
             super.configure(web);
         } catch (Exception e) {
-            logger.info(e.getLocalizedMessage(),e);
+            logger.info(e.getLocalizedMessage(), e);
         }
     }
 }
