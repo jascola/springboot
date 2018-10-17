bacead0f16db261d07f9680eee01eff88e559409
jascola <1947857236@qq.com>
2018-10-17 16:42:38 +0800

    spring security

--- src/main/java/com/jascola/model/entity/UserEntity.java
+++ src/main/java/com/jascola/model/entity/UserEntity.java
@@ -4,37 +4,22 @@ import java.io.Serializable;
 
 public class UserEntity implements Serializable {
     private static final long serialVersionUID = 1L;
-    /**
-     * 电话号码
-     */
-    private String phone;
+
     /**
      * 姓名
      */
-    private String name;
+    private String username;
     /**
      * 密码（MD5加密）
      */
     private String password;
-    /**
-     * 邮箱
-     */
-    private String email;
 
-    public String getPhone() {
-        return phone;
+    public String getUsername() {
+        return username;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
@@ -43,23 +28,5 @@ public class UserEntity implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "UserEntity{" +
-                "phone='" + phone + '\'' +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                '}';
     }
 }
--- src/main/java/com/jascola/springboot/conf/WebSecurityConfig.java
+++ src/main/java/com/jascola/springboot/conf/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.jascola.springboot.conf;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,11 +16,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private static final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
+    @Autowired
+    BasicDataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) {
         try {
-            http.authorizeRequests().antMatchers("/**").hasRole("ADMIN").and().formLogin().and().httpBasic();
+            http.authorizeRequests().anyRequest().authenticated()
+                    .and().formLogin().permitAll();
+
         } catch (Exception e) {
             logger.info(e.getLocalizedMessage(), e);
         }
@@ -27,9 +33,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) {
         try {
-
-            auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder()).withUser("jascola").
-                    password(new BCryptPasswordEncoder().encode("123456")).roles("ADMIN");
+            /*auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder()).withUser("jascola").
+                    password(new BCryptPasswordEncoder().encode("123456")).roles("ADMIN");*/
+            auth.jdbcAuthentication().dataSource(dataSource).
+                    usersByUsernameQuery("select username,password, enabled from users where username = ?").
+                    authoritiesByUsernameQuery("select username,role from roles where username = ?").
+                    passwordEncoder(new BCryptPasswordEncoder());
         } catch (Exception e) {
             logger.info(e.getLocalizedMessage(), e);
 
